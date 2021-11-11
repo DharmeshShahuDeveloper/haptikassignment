d0598800478a41a7794a1464f74523c408640916
Dharmesh Shahu <dharmeshshahu@Dharmeshs-MBP.bbrouter>
2021-11-11 15:31:21 +0530

    done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={'/haptikassignment'}>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
