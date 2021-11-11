4760b6eb9e2019e1c1ddff33cdcfba62edec847a
Dharmesh Shahu <dharmeshshahu@Dharmeshs-MBP.bbrouter>
2021-11-11 16:11:48 +0530

    modal file issue

--- src/Modal.js
+++ src/Modal.js
@@ -1,5 +1,5 @@
+import { useEffect, useState } from 'react';
 import { createPortal } from "react-dom";
-import { useEffect } from "react/cjs/react.development";
 
 export default function Modal(props) {
 
